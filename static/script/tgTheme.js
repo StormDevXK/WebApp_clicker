7e8e4fc30fde3ade954f845285776911278be7cc
SrormDevXK <mhstudio1912@gmail.com>
2024-01-18 00:43:15 +0300

    tg theme

---
+++ static/script/tgTheme.js
@@ -0,0 +1,7 @@
+let tg = window.Telegram.WebApp;
+
+let body = document.body
+body.style.backgroundColor = tg.ThemeParams.bg_color
+body.style.color = tg.ThemeParams.text_color
+document.querySelector('.clickButton').style.backgroundColor = tg.ThemeParams.button_color
+document.querySelector('.clickButton').style.color = tg.ThemeParams.button_text_colorString
