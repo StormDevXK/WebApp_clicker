6817593099e0e39205bd087afb1e23ef9bbd4497
SrormDevXK <mhstudio1912@gmail.com>
2024-01-18 00:55:29 +0300

    ws dynamic port for client js

--- static/script/script.js
+++ static/script/script.js
@@ -1,7 +1,7 @@
 const clickButton = document.querySelector('.clickButton')
 const score = document.querySelector('.scoreText')
 
-const socket = new WebSocket('ws://192.168.3.15:5051')
+const socket = new WebSocket("ws://" + window.location.hostname + ':5051')
 // const socket = new WebSocket('ws://127.0.0.1:5051')
 socket.addEventListener('open', () => {
     console.log('Соединение')
