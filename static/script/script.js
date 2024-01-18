957f681771149bf72a7012bd389a6b43524680a7
SrormDevXK <mhstudio1912@gmail.com>
2024-01-19 02:15:49 +0300

    score update

--- static/script/script.js
+++ static/script/script.js
@@ -2,10 +2,10 @@ let tg = window.Telegram.WebApp
 const clickButton = document.querySelector('.clickButton')
 const score = document.querySelector('.scoreText')
 
-//const wsAdr = 'ws://' + window.location.hostname + ':5051'
+const wsAdr = 'ws://' + window.location.hostname + ':5051'
 //console.log(wsAdr)
-//const socket = new WebSocket(wsAdr)
-const socket = new WebSocket('ws://ws.stormdevxk.ru:5051')
+const socket = new WebSocket(wsAdr)
+// const socket = new WebSocket('ws://localhost:5051')
 socket.addEventListener('open', () => {
     console.log('Соединение')
 })
@@ -17,8 +17,8 @@ socket.addEventListener('message', (mes) => {
 function clickButtonDown(e) {
     clickButton.style.backgroundColor = 'green'
     socket.send(JSON.stringify('plus'))
-    score.innerHTML = (parseInt(score.innerHTML)+1).toString()
     tg.HapticFeedback.impactOccurred('medium')
+    score.innerHTML = (parseInt(score.innerHTML)+1).toString()
 }
 function clickButtonUp(e) {
     clickButton.style.backgroundColor = 'var(--tg-theme-button-color)'
