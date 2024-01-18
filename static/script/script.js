4d88c8aa7cdb2457ac9a9bdb0e36dda6f03a969c
main <main@2040107-cq89736.twc1.net>
2024-01-19 02:01:34 +0300

    theme work normaly

--- static/script/script.js
+++ static/script/script.js
@@ -1,12 +1,14 @@
+let tg = window.Telegram.WebApp
 const clickButton = document.querySelector('.clickButton')
 const score = document.querySelector('.scoreText')
 
 function clickButtonDown(e) {
     clickButton.style.backgroundColor = 'green'
     score.innerHTML = (parseInt(score.innerHTML)+1).toString()
+    tg.HapticFeedback.impactOccurred('medium')
 }
 function clickButtonUp(e) {
-    clickButton.style.backgroundColor = 'red'
+    clickButton.style.backgroundColor = 'var(--tg-theme-button-color)'
 }
 
 // clickButton.addEventListener('mousedown', clickButtonDown)
---
+++ static/script/tgTheme.js
@@ -0,0 +1,6 @@
+let body = document.body
+body.style.backgroundColor = tg.backgroundColor
+document.querySelector('.scoreText').style.color = 'var(--tg-theme-text-color)'
+//body.style.color = 'var(--tg-theme-hint-color)'
+document.querySelector('.clickButton').style.backgroundColor = 'var(--tg-theme-button-color)'
+document.querySelector('.clickButton').style.color = 'var(--tg-theme-button-text-color)'
